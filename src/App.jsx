fc74facd1e52bf54d5c53dbdb3c6cf6eefb853d7
Thomas <boy1.thoomas@gmail.com>
2025-03-31 11:31:39 +0200

    feat(app): add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 export const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(error));
+  });
   return (
-    <>
-    </>
-  )
-}
+    <Container className="mt-4">
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} md={4} lg={3} className="mb-4">
+            <Card className="h-100">
+              <Card.Img
+                src={product.image}
+                style={{ height: "300px", objectFit: "contain" }}
+              ></Card.Img>
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>{product.price} €</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
