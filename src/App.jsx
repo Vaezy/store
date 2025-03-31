e08fedf42679f7b710906b55a4d9b584f743dc26
Thomas <boy1.thoomas@gmail.com>
2025-03-31 11:48:28 +0200

    refactor(products): new product loading logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import { useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
+import "./App.scss";
 
 export const App = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error(error));
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
   });
   return (
     <Container className="mt-4">
       <Row>
         {products.map((product) => (
           <Col key={product.id} md={4} lg={3} className="mb-4">
-            <Card className="h-100">
+            <Card className="product-card">
               <Card.Img
                 src={product.image}
-                style={{ height: "300px", objectFit: "contain" }}
+                className="product-image"
               ></Card.Img>
               <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
+                <Card.Text className="text-muted">
+                  {product.description}
+                </Card.Text>
                 <Card.Text>{product.price} €</Card.Text>
               </Card.Body>
             </Card>
